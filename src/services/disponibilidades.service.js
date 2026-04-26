1cc85396ffc98af36f0d55e5515c2c7e5171ad9a
Santi Gorbea <sgorbea@frba.utn.edu.ar>
2026-04-26 20:04:02 -0300

    Medicos y disponibilidad funcionando correctamente

--- src/services/disponibilidades.service.js
+++ src/services/disponibilidades.service.js
@@ -6,39 +6,42 @@ import MedicoService from "./medicos.service.js";
 const medicoService = new MedicoService();
 
 export default class DisponibilidadesService {
- async getByMedico(idMedico){
+  async getByMedico(idMedico) {
     const medico = await medicoService.getById(idMedico);
     return medico.disponibilidad;
   }
 
   async getById(idMedico, id) {
     const medico = await medicoService.getById(idMedico);
-    const disp = await medico.disponibilidad.find(disp => disp.id == id);
-    if(!disp) throw new NotFoundError("Disponibilidad no encontrada");
+    const disp = await medico.disponibilidad.find((disp) => disp.id == id);
+    if (!disp) throw new NotFoundError("Disponibilidad no encontrada");
     return disp;
   }
 
   async create(idMedico, nuevaDisponibilidad) {
     const medico = await medicoService.getById(idMedico);
-    const newMedico = medicoService.update(idMedico, { disponibilidad: [ ...medico.disponibilidad, nuevaDisponibilidad ] })
+
+    const newMedico = await medicoService.update(idMedico, {
+      disponibilidad: [...medico.disponibilidad, nuevaDisponibilidad],
+    });
     return newMedico.disponibilidad;
   }
 
   async update(idMedico, idDisp, disponibilidadActualizada) {
-    const newMedico = medicoService.update(idMedico, { disponibilidad: [ { idDisp: nuevaDisponibilidad } ] })
+    const medico = await medicoService.getById(idMedico);
+    const newMedico = await medicoService.update(idMedico, {
+      disponibilidad: medico.disponibilidad.map((disp) =>
+        disp.id == idDisp ? disponibilidadActualizada : disp,
+      ),
+    });
     return newMedico.disponibilidad;
   }
 
-  /*Falta actualizar esta funcion*/
   async delete(idMedico, idDisp) {
-    const medico = medicoRepository.getById(idMedico);
-
-    if (!medico) throw new Error("Médico no encontrado");
-
-    const index = disponibilidadesDB.findIndex((m) => m.id === parseInt(id));
-    if (index === -1) throw new Error("Disponibilidad no encontrada");
-
-    const disponibilidadEliminada = disponibilidadesDB.splice(index, 1);
-    return disponibilidadEliminada[0];
+    const medico = await medicoService.getById(idMedico);
+    const newMedico = await medicoService.update(idMedico, {
+      disponibilidad: medico.disponibilidad.filter((disp) => disp.id != idDisp),
+    });
+    return newMedico.disponibilidad;
   }
 }
