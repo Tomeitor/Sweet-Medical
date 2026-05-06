6c497d0ed27f0c27cb84b563664a9dd9f2b758b7
tomeitor <tomycarnezis@gmail.com>
2026-05-06 17:30:01 -0300

    Fix de los enums duplicados creo

--- src/domain/diaSemana.js
+++
@@ -1,9 +0,0 @@
-export const DiaSemana = Object.freeze({
-    DOMINGO: 'DOMINGO',
-    LUNES: 'LUNES',
-    MARTES: 'MARTES',
-    MIERCOLES: 'MIERCOLES',
-    JUEVES: 'JUEVES',
-    VIERNES: 'VIERNES',
-    SABADO: 'SABADO'
-});
--- src/domain/estadoTurno.js
+++
@@ -1,7 +0,0 @@
-export const EstadoTurno = Object.freeze({
-    DISPONIBLE: 'DISPONIBLE',
-    RESERVADO: 'RESERVADO',
-    CONFIRMADO: 'CONFIRMADO',
-    CANCELADO: 'CANCELADO',
-    REALIZADO: 'REALIZADO'
-});
